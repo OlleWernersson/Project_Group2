30541294fed1c53afd735fc448b1ee2876a9ed97
OlleWernersson <Olle.Wernersson.3072@student.uu.se>
2023-11-22 10:29:09 +0100

    FirstCommit

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,15 +1,23 @@
 <template>
   <header>
-    <div v-bind:class="['hamburger', {'close': !hideNav}]" 
+<!--     <div v-bind:class="['hamburger', {'close': !hideNav}]" 
          v-on:click="toggleNav">
     </div>
     <div class="logo">
-      <img src="/img/logo.png">
       Polly polling tool 
-      <img src="../assets/logo.svg">
-    </div>
+    </div> -->
   </header>
-  <ResponsiveNav v-bind:hideNav="hideNav">
+  <main>
+    <section class="supportButtons">
+      <button class="language-button" v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
+      <button class="help-button">Help</button>
+    </section>
+    <section class="mainButtons">
+      <router-link class="main-button" to="/join/">{{uiLabels.joinGame}}</router-link>
+      <router-link class="main-button" to="/create/">{{uiLabels.createGame}}</router-link>
+    </section>
+  </main>
+<!--   <ResponsiveNav v-bind:hideNav="hideNav">
     <button v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
     <router-link to="/create/">{{uiLabels.createPoll}}</router-link>
     <a href="">{{uiLabels.about}}</a>
@@ -21,7 +29,7 @@
     Write poll id: 
     <input type="text" v-model="id">
   </label>
-  <router-link v-bind:to="'/poll/'+id">{{uiLabels.participatePoll}}</router-link>
+  <router-link v-bind:to="'/poll/'+id">{{uiLabels.participatePoll}}</router-link> -->
 </template>
 
 <script>
@@ -38,13 +46,13 @@ export default {
     return {
       uiLabels: {},
       id: "",
-      lang: localStorage.getItem("lang") || "en",
+      lang: localStorage.getItem("lang") || "en", //Use localstorage sparsly to have it responsive
       hideNav: true
     }
   },
-  created: function () {
+  created: function () { //Created Hook, Before the content of the page is loaded the creaded hook is run
     socket.emit("pageLoaded", this.lang);
-    socket.on("init", (labels) => {
+    socket.on("init", (labels) => { //you should probablt have all the listeners in this created hook.
       this.uiLabels = labels
     })
   },
@@ -66,7 +74,45 @@ export default {
 }
 </script>
 <style scoped>
-  header {
+.supportButtons {
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  margin-top: 4%;
+}
+
+.language-button {
+  order: 1; /* Language button on the left */
+  margin-left: 4%;
+}
+
+.help-button {
+  order: 2; /* Help button on the right */
+  margin-right: 4%;
+}
+
+.mainButtons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh; /* Adjust the height as needed */
+}
+.main-button {
+  display: inline-block;
+  padding: 1em 2em; /* Adjust padding as needed */
+  font-size: 1.2em; /* Adjust font size as needed */
+  background-color: plum; /* Green background color */
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 0.5em;
+  cursor: pointer;
+  border-radius: 8px;
+}
+/*   header {
     background-color: gray;
     width: 100%;
     display: grid;
@@ -114,5 +160,5 @@ export default {
   .hide {
     left:-12em;
   }
-}
+} */
 </style>
