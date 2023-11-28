88e03918487e63ad3aa26884550e9c4f7ae5feb9
OlleWernersson <Olle.Wernersson.3072@student.uu.se>
2023-11-28 20:24:34 +0100

    BildOchPlayerList

---
+++ src/components/PlayerList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="player-list">
+    <div v-for="player in players" :key="player.id" class="player-box">
+      {{ player.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    players: Array, // Assume players is an array of objects with 'id' and 'name'
+  },
+};
+</script>
+
+<style scoped>
+.player-list {
+  margin-top: 20px;
+}
+
+.player-box {
+  padding: 10px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+</style>
