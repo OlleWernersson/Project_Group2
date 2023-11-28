192ea22538cd3f6c6d31d4e9b240811586b239f9
AAlbin564 <albintheo@gmail.com>
2023-11-28 14:46:36 +0100

    update 2

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,6 +1,22 @@
-<template>
-hehj
+<template >
+    <div id="rescaler "></div>
+    <div id="background">
+        <p>hej</p>
+        <div ></div>
+    </div>
 </template>
 <script>
+export default{
+
+}
 </script>
-<style></style>
+<style>
+#background{
+    background-image: url('polacks.jpg');
+    height: 1000px;
+    position: absolute;
+}
+#rescaler{
+    position: relative;
+}
+</style>
