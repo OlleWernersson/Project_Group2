10cb32b2cc4fd7deefbcef8a91b233c91e6936b6
Maja Danielsson Kadestam <majadakad@MBPsomthorMaja4.lan>
2023-12-05 21:48:11 +0100

    map component created for gameview

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="map">
+        <div id="dots" :style="{ left: dotLeft + 'px', top: dotTop + 'px' }">T</div>
+        
+        <div id="cities" v-for="city in selectedcities" :style="{left: city.left + 'px',top:city.top  + 'px'}"> {{ city.Bletter }}</div>
+        
+        <button @click="moveMeForward"> move me! </button>   
+        
+      </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            dotTop: 100,
+            dotLeft: 500,
+            displayWrongSign: false,
+            cityLeft: 500,
+            cityTop: 200,
+            selectedcities:[
+                {top:500, left:200, Bletter: 'A'},
+                {top:100, left:200, Bletter: 'B'},
+                {top:200, left:500, Bletter: 'C'}
+            ],
+            currentCity: 0,
+            }
+        },
+
+        methods: {
+        moveMeForward() {
+            this.dotLeft = this.selectedcities[this.currentCity].left;
+            this.dotTop = this.selectedcities[this.currentCity].top -20;
+            this.currentCity++;
+        },
+        },
+    }
+</script>
+
+<style scoped>
+  #map {
+    background-image: url('../public/img/polacks.jpg');
+    width: 100%;
+    height: 26em;
+    position: relative;
+    font-size: 2em;
+  }
+  
+  #dots {
+    background-color: blue;
+    border-radius: 15px;
+    position: absolute;
+    font-size: 1em;
+    width: 35px;
+  }
+  #cities{
+    position:absolute;
+  }
+</style>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,13 +1,9 @@
 <template>
     <div id="wrapper">
-      <div id="map">
-        
-        <div id="dots" :style="{ left: dotLeft + 'px', top: dotTop + 'px' }">T</div>
-        
-        <div id="cities" v-for="city in selectedcities" :style="{left: city.left + 'px',top:city.top  + 'px'}"> {{ city.Bletter }}</div>
-        
-        <button @click="moveMeForward"> move me! </button>
-      
+      <div> 
+        <Map>
+          <area shape="rect" coords="0, 0, 100, 100">
+        </Map> 
       </div>
      
       <div> <Question id = "buttonWrapper" :question = "question"/> </div> 
@@ -17,36 +13,14 @@
   
   <script>
   import Question from '../components/QuestionComponent.vue';
+  import Map from '../components/MapComponent.vue';
 
   export default {
-    components: { Question },
+    components: { Question, Map},
     data() {
       return {
-        dotTop: 100,
-        dotLeft: 500,
-        displayWrongSign: false,
-        cityLeft: 500,
-        cityTop: 200,
-        selectedcities:[
-            {top:500, left:200, Bletter: 'A'},
-            {top:100, left:200, Bletter: 'B'},
-            {top:200, left:500, Bletter: 'C'}
-    ],
-    currentCity: 0,
-    question: 'This is a question, whats your answer?'
-      };
-    },
-  
-    methods: {
-      moveMeForward() {
-        this.dotLeft = this.selectedcities[this.currentCity].left;
-        this.dotTop = this.selectedcities[this.currentCity].top -20;
-        this.currentCity++;
-      },
-  
-      wrongAnswer() {
-        
-      },
+        question: 'This is a question, whats your answer?'
+        };
     },
   };
   </script>
@@ -55,25 +29,6 @@
   #wrapper {
     display: grid;
     grid-template-columns: 1fr 1fr;
-  }
-  
-  #map {
-    background-image: url('../public/img/polacks.jpg');
-    width: 100%;
-    height: 26em;
-    position: relative;
-    font-size: 2em;
-  }
-  
-  #dots {
-    background-color: blue;
-    border-radius: 15px;
-    position: absolute;
-    font-size: 1em;
-    width: 35px;
-  }
-  #cities{
-    position:absolute;
   }
   </style>
   
