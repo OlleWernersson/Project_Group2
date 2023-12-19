17fe9071bd2c5fa6e0e5f6f4d470dbe456cae137
Emma Fredriksson <emmafredriksson@MacBook-Pro-som-tillhor-Emma.local>
2023-12-19 12:07:05 +0100

    player piece

--- src/views/joinView.vue
+++ src/views/joinView.vue
@@ -7,6 +7,16 @@
     <label>
       <input type="text" v-model="gameID" :placeholder="uiLabels.gameId" @input="handleGameIDInput" autocomplete="off">
     </label>
+
+    <label class="player-pieces-box">
+      <p class="choose-color-text">{{ uiLabels.choosecolor }}</p>
+      <div class="player-pieces">
+        <div class="player-piece" v-for="(color, index) in playerColors" :key="index" @click="selectPlayerPiece(color)">
+          <div class="piece-circle" :style="{ backgroundColor: color }"></div>
+        </div>
+      </div>
+    </label>
+
   </main>
   <router-link id="join-lobby-button" class="main-button" :to="'/lobby/' + gameID" tag="button">{{ uiLabels.joinLobby}}</router-link>
 </template>
@@ -24,6 +34,8 @@ export default {
       playerName: "",
       gameID: "",
       lang: localStorage.getItem("lang") || "en",
+      playerColors: ["#e84a5f", "#3baea0", "#085f63", "#facf5a", "#ff8b00", "#ff847c", "#7481cf"],
+      selectedPlayerPiece: null,
     };
   },
   created: function () {
@@ -42,8 +54,12 @@ export default {
     handleNameInput() {
       // Om vi vill ha en limit på spelarnamn
       this.playerName = this.playerName.slice(0,20)
-    }
+    },
+    selectPlayerPiece(color) {
+    // Hanterar valet av spelpjäs
+    this.selectedPlayerPiece = color; 
   },
+},
 };
 </script>
 
@@ -75,4 +91,38 @@ input:focus {
   bottom: 0;
   right: 0;
 }
+
+.player-pieces-box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+    border: 2px solid pink;
+    border-radius: 8px;
+    width: 300px;
+    font-size: 1.5em;
+    margin-top: 10px;
+    background-color:floralwhite;
+    outline: none; 
+  }
+
+  .choose-color-text {
+    color: rgb(164, 161, 161);
+}
+.player-pieces {
+    display: flex;
+    margin-top: 2px;
+    margin-bottom: 20px;
+  }
+
+  .player-piece {
+    margin-right: 10px;
+    cursor: pointer;
+  }
+
+  .piece-circle {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+  }
 </style>
