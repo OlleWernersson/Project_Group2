b98ee83ee79ef145863dd91efef8f0211468b4d8
Maja Danielsson Kadestam <majadakad@student-212-208.eduroam.uu.se>
2023-12-19 10:52:46 +0100

    citiesfix

---
+++ src/components/CityComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    
+    <p class="citystyle" v-for="city in cities" :key="city.name">{{ city.name }}</p>
+
+</template>
+
+<script>
+export default{
+    props:{
+        cities: Object
+    },
+    data() {
+        return {
+                isSelected: true,
+        };
+    },
+    methods: {
+        select: function () {
+        this.isSelected = !this.isSelected;
+  },
+},
+
+}
+</script>
+
+<style>
+    .citystyle{
+        background-color: aqua;
+        border-radius: 10px;
+    }
+</style>
+
+
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -6,14 +6,16 @@
     <Question @correctAnswerClick = "sendButtonClicktoMap" id = "buttonWrapper" :question = "question"/> 
     <!-- När Question  emittar ett correctAnswerClick (custom event) anropas metoden sendButtonClciktoMap
     i GameView -->
-<City> Hej</City> 
   </div>
+
+  <City :cities = "cities" > </City>
 </template>
 
 <script>
 import Question from '@/components/QuestionComponent.vue';
 import Map from '../components/MapComponent.vue';
-import City from '../components/City.vue';
+import City from '../components/CityComponent.vue';
+import cities from '../../server/data/cities.json';
 
 import io from 'socket.io-client';
 const socket = io("localhost:3000");
@@ -22,7 +24,8 @@ export default {
   components: {Map, Question, City},
   data() {
     return {
-      question: 'This is a question, whats your answer?'
+      question: 'This is a question, whats your answer?',
+      cities: cities
       };
   },
 
